520956df3a3eac3a8d63389bb5e9a88cd4cf947f
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-02-24 23:25:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+        function appendCharacter(char) {
+            document.getElementById("display").value += char;
+        }
+        function clearDisplay() {
+            document.getElementById("display").value = "";
+        }
+        function backspace() {
+            let display = document.getElementById("display");
+            display.value = display.value.slice(0, -1);
+        }
+        function calculateResult() {
+            try {
+                document.getElementById("display").value = eval(document.getElementById("display").value);
+            } catch {
+                document.getElementById("display").value = "Error";
+            }
+        }
+    </script>
